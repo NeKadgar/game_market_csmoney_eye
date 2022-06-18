6834a81e7fea448f1f638d07e27c5c3c2cc597f2
Maxim Sheleh <maximsheleh@gmail.com>
2022-06-18 18:58:44 +0300

    aws deploy test

--- background/celery_app.py
+++ background/celery_app.py
@@ -19,7 +19,7 @@ app.conf.beat_schedule = {
     # Executes every 2 hours
     "parse": {
         "task": "background.tasks.parser.parse",
-        "schedule": timedelta(hours=4),
+        "schedule": timedelta(hours=1),
         'options': {
             'queue': 'csmoney_queue',
             # 'expires': timedelta(seconds=5),
